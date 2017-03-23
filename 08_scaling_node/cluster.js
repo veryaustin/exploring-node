bd829e821af0eb865f5e965bfec7fe62bc8fefdd
Austin Lauritsen <austin@veryaustin.com>
2017-03-23 14:24:33 -0500

    A clustered node server that each node responds only 3 times before termination

---
+++ 08_scaling_node/cluster.js
@@ -0,0 +1,43 @@
+var cluster = require('cluster');
+var http = require('http');
+var numWorkers = 2;
+
+if (cluster.isMaster) {
+  // Fork Worders
+  for (var i = 0; i < numWorkers; i++) {
+    console.log('master: about to fork a worker');
+    cluster.fork();
+  }
+
+  cluster.on('fork', function(worker) {
+    console.log('master: fork event (worker ' + worker.id + ')');
+  });
+
+  cluster.on('online', function(worker) {
+    console.log('master: online event (worker ' + worker.id + ')');
+  });
+
+  cluster.on('listening', function(worker, address) {
+    console.log('master: listening event (worker ' + worker.id + ', pid ' + worker.process.pid + ', ' + address.address + ':' + address.port + ')');
+  });
+
+  cluster.on('exit', function(worker) {
+    console.log('master: listening event (worker ' + worker.id + ')');
+  });
+} else {
+
+  var count = 0;
+  
+  // Workers can share any TCP connection
+  // In this case it's HTTP
+  http.createServer(function(req, res) {
+    res.writeHead(200);
+    
+    count++;
+    console.log("Worker #" + cluster.worker.id + " is incrementing count to " + count);
+    res.end("hello world from worker #" + cluster.worker.id + " (pid " + cluster.worker.process.pid + ") with count = " + count + "\n");
+    if (count === 3) {
+      cluster.worker.destroy();
+    }
+  }).listen(process.env.PORT, process.env.IP);
+}
